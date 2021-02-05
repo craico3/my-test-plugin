9bf12c4acd1ed35c7b235f1efea8c0b387c1e54d
craico3 <craico3@gmail.com>
2021-02-05 11:09:58 -0500

    First commit

---
+++ my-test-plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: My Test Plugin
+Plugin URI: http://wordpressplugincourse.com/plugins/snappy-list-builder
+Description: This is a test plugin for WordPress
+Version: 1.0
+Author: Joel Funk @ Code College
+Author URI: http://joelfunk.codecollege.ca
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: my-snappy-list-builder
+*/
