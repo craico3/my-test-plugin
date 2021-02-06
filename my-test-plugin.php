23723d07c01a4338d7df2357ec4513d45c98e2ac
craico3 <craico3@gmail.com>
2021-02-06 06:22:33 -0500

    Make custom URL available

--- my-test-plugin.php
+++ my-test-plugin.php
@@ -11,3 +11,29 @@ License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: my-snappy-list-builder
 */
+
+// make custom url available
+add_action('init', 'my_url_handler');
+
+function my_url_handler()
+{
+  $uri = explode('?', $_SERVER['REQUEST_URI'], 2)[0];
+  if ($uri === '/test/myurl') {
+    echo "Hellohellohellohellohellohellohellohellohellohellohellohellohello<br />";
+    echo "Hellohellohellohellohellohellohellohellohellohellohellohellohello<br />";
+    echo "Hellohellohellohellohellohellohellohellohellohellohellohellohello<br />";
+    echo "Hellohellohellohellohellohellohellohellohellohellohellohellohello<br />";
+    echo "Hellohellohellohellohellohellohellohellohellohellohellohellohello<br />";
+    echo "Hellohellohellohellohellohellohellohellohellohellohellohellohello<br />";
+    echo "Hellohellohellohellohellohellohellohellohellohellohellohellohello<br />";
+    echo "Hellohellohellohellohellohellohellohellohellohellohellohellohello<br />";
+    echo "Hellohellohellohellohellohellohellohellohellohellohellohellohello<br />";
+    echo "Hellohellohellohellohellohellohellohellohellohellohellohellohello<br />";
+    echo "Hellohellohellohellohellohellohellohellohellohellohellohellohello" . $uri;
+    if (isset($_GET['usrtbldata']) && $_GET['usrtbldata'] == 1) {
+      echo "<h1>TEST</h1>";
+      exit;
+    }
+  }
+}
+
